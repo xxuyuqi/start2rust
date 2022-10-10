0f601b9a5d4942aefb373dc5ff2acba40813f1f7
xxuyuqi <uiaiuoh@outlook.com>
2022-10-10 16:26:19 +0800

    function

--- function/src/main.rs
+++ function/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+    let _x = example_func(2 ,3);
+    let _y = {
+        let x = 3;
+        x + 1
+    };
+
+    no_return();
+    
+    // diverge function
+    fn forever() -> ! {
+        loop {
+          //...
+        };
+}
+
+
+// prototype of a function
+// keyword identifier(paramenters) -> return type {
+// a function can be placed everywhere
+fn example_func(i: i32, j: i32) -> i32
+{
+    let _x = 1; // statement don't return a value
+    // return 5_i32 function can return early by return keyword.
+    i + j // expression return a value
+    // expression can't end with semicolon
+}
+
+fn no_return() -> () {
+    println!("no return.")
 }
