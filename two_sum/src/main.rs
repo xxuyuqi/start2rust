4c394ea895106f38343326fbf9ca99dc1be5eedc
xxuyuqi <uiaiuoh@outlook.com>
2022-10-11 17:12:13 +0800

    add new code

--- two_sum/src/main.rs
+++ two_sum/src/main.rs
@@ -1,4 +1,24 @@
+struct Solution {}
+
+use std::collections::HashMap;
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map:HashMap<i32,usize>=HashMap::new();
+        for (i, num) in nums.iter().enumerate() {
+            let k=target-num;          
+            if let Some(ii) = map.get(&k){
+                if *ii != i{
+                    return vec![*ii as i32,i as i32];
+                }                
+            }
+             map.insert(*num, i);
+        }        
+        vec![]
+    }
+}
+
 fn main() {
-    let mut s = "abcd";
-    s = "efg";
+    let nums = vec![3,2,4];
+    let res = Solution::two_sum(nums, 6);
+    println!("{:?}", res);
 }
